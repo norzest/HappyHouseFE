144a40c8e5aaf596fb9711361c2dbd460d92d577
영효 심 <dark1203@naver.com>
2022-05-19 15:28:14 +0900

    jwtToken

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,25 @@ import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
+// // https://router.vuejs.org/kr/guide/advanced/navigation-guards.html
+// const onlyAuthUser = async (to, from, next) => {
+//   // console.log(store);
+//   const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+//   const getUserInfo = store._actions["memberStore/getUserInfo"];
+//   let token = sessionStorage.getItem("access-token");
+//   if (checkUserInfo == null && token) {
+//     await getUserInfo(token);
+//   }
+//   if (checkUserInfo === null) {
+//     alert("로그인이 필요한 페이지입니다..");
+//     next({ name: "signIn" });
+//     // router.push({ name: "signIn" });
+//   } else {
+//     // console.log("로그인 했다.");
+//     next();
+//   }
+// };
+
 const routes = [
   {
     path: "/",
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ token }}</h1>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      token: null,
+    };
+  },
+  created() {
+    axios.get("http://localhost:8080/happyhouse/test/api").then(({ data }) => {
+      this.token = data;
+    });
+  },
+};
+</script>
