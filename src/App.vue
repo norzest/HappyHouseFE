ac7d5f9c90f3052e7b754cc9076755e099c65f5e
yb8350 <yb8350@naver.com>
2022-05-19 13:02:05 +0900

    Update

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,6 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+@import url("@/assets/css/reset.css");
+</style>
--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,15 +1,21 @@
 <template>
   <header>
     <div id="logo">
-      <router-link to="/">logo</router-link>
+      <router-link to="/">
+        <img src="../assets/img/logo_happyhouse_white.png" alt="" />
+      </router-link>
     </div>
     <div id="navbar">
       <router-link to="/about">About</router-link>
       <router-link to="/about">About</router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <div id="user">
-      <router-link to="/about">user</router-link>
+    <div v-if="true" id="guest">
+      <router-link to="/about">login</router-link>
+      <router-link to="/about">join</router-link>
+    </div>
+    <div v-else id="user">
+      <div class="user-img"></div>
     </div>
   </header>
 </template>
@@ -18,22 +24,51 @@
 export default {};
 </script>
 
-<style>
+<style scope>
 header {
   width: 100%;
   background-color: #996a54;
-
-  height: 100px;
+  height: 80px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  font-size: 24px;
+  font-size: 20px;
 }
+
 header a {
-  padding: 10px;
+  color: white;
+}
+
+header #logo,
+header #guest {
+  padding: 0 20px;
+}
+#logo img {
+  width: 90px;
 }
 
 #navbar {
+  flex-grow: 1;
+  padding: 0 10px;
   text-align: left;
+}
+#navbar a {
+  display: inline-block;
+  padding: 0 10px;
+}
+#guest a {
+  display: inline-block;
+  padding: 0 10px;
+}
+
+@media screen and (max-width: 768px) {
+  #navbar,
+  #guest,
+  #user {
+    display: none;
+  }
+
+  #logo img {
+  }
 }
 </style>
