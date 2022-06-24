744820197003f5d5eb3fb4a368c8bfbc03b3bfed
영효 심 <dark1203@naver.com>
2022-06-24 17:19:17 +0900

    security

--- src/config/index.js
+++
@@ -1,5 +0,0 @@
-const API_BASE_URL = "http://localhost:8080/happyhouse";
-const KAKAO_MAP_URL =
-  "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=daa5ceef8e2d3962bdb57f5aa30c0356&libraries=clusterer,services";
-
-export { API_BASE_URL, KAKAO_MAP_URL };
